a9aa19914366c21497b3f4485f1f2bde15c75c8c
Jiao <jiaojiaobigbang@gmail.com>
2024-07-21 13:07:15 +1200

    controller update, created database for posts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                @for ($i = 0; $i <= 10; $i++)
+                    <div class="card">
+                        <div class="card-header">Title</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                        <div class="card-body">
+                            Content
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+                    </div>
+                @endfor
             </div>
         </div>
     </div>
-</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-
-
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('/posts',App\Http\Controllers\PostController::class)->middleware('auth');
