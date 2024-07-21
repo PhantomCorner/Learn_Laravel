5d600acc502cfb221b99c01aaeeb4ee0331de419
Jiao <jiaojiaobigbang@gmail.com>
2024-07-21 13:15:38 +1200

    post view update

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -4,8 +4,19 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <h2>Create new posts</h2>
+                <form>
+                    <div class="form-group">
+                        <label for='title'>Title</label>
+                        <input type="text" class='form-control' id='title' name='title'>
+                    </div>
+                    <div class="form-group">
+                        <label for='content'>Content</label>
+                        <textarea class="form-control" id="content" name="content" rows='5'></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary"> Submit</button>
             </div>
+            </form>
         </div>
+    </div>
     </div>
 @endsection
